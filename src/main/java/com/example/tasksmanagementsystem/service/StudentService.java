c1f0a34d97335aa1c9d363479c4405f4cb1d5e32
Evgeni-Georgiev <evgenigg2000@gmal.com>
2022-11-06 17:15:39 +0200

    Rewrote service classes to implement interfaces, work with DTO pattern and use custom exceptions.

--- src/main/java/com/example/tasksmanagementsystem/service/StudentService.java
+++ src/main/java/com/example/tasksmanagementsystem/service/StudentService.java
@@ -1,17 +1,21 @@
 package com.example.tasksmanagementsystem.service;
 
+import com.example.tasksmanagementsystem.dto.StudentUpdateDto;
+import com.example.tasksmanagementsystem.exception.DuplicateEntityException;
+import com.example.tasksmanagementsystem.exception.ResourceNotFoundException;
 import com.example.tasksmanagementsystem.model.Student;
 import com.example.tasksmanagementsystem.repository.StudentRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
-// Here is the business logic
 @Service
-public class StudentService {
+@Transactional
+@Slf4j
+public class StudentService implements StudentInterface {
 
 	private final StudentRepository studentRepository;
 
@@ -20,65 +24,57 @@ public class StudentService {
 		this.studentRepository = studentRepository;
 	}
 
+	@Override
 	public List<Student> getStudents() {
-		// instead of returning a list, do:
 		return studentRepository.findAll();
 	}
 
-	public Optional<Student> getStudentById(Long studentId) {
+	@Override
+	public Student getStudentById(Long studentId) {
 		return studentRepository.findStudentById(studentId);
 	}
 
-	public void addNewStudent(Student student) {
+	@Override
+	public Long addNewStudent(Student student) {
 		Optional<Student> studentOptional = studentRepository
 			.findStudentByEmail(student.getEmail());
 		if(studentOptional.isPresent()) {
-			throw new IllegalStateException("Email is taken");
+			throw new DuplicateEntityException("User with this email already exists.");
 		}
 		studentRepository.save(student);
+		return student.getId();
 	}
 
+	@Override
 	public void deleteStudent(Long studentId) {
 		boolean exists = studentRepository.existsById(studentId);
 		if(!exists) {
-			throw new IllegalStateException(
-				"student with id " + studentId + " does not exists"
+			throw new ResourceNotFoundException(
+				"Student with id " + studentId + " does not exists"
 				);
 		}
 		studentRepository.deleteById(studentId);
 	}
 
+	@Override
 	@Transactional
-	public void updateStudent(Long studentId,
-							  String name,
-							  String email,
-							  Long phone,
-							  String address,
-							  String picture
-							  ) {
+	public void updateStudent(Long studentId, StudentUpdateDto studentUpdateDto) {
 		Student student = studentRepository.findById(studentId)
-			.orElseThrow(() -> new IllegalStateException(
-				"student width id " + studentId + "does not exists."
+			.orElseThrow(() -> new ResourceNotFoundException(
+				"Student width id " + studentId + " does not exists."
 			));
-		if(name != null
-			&& name.length() > 0
-			&& !Objects.equals(student.getName(), name)) {
-			student.setName(name);
-		}
-
-		Optional<Student> studentOptional = studentRepository
-			.findStudentByEmail(email);
-		if(email != null
-			&& email.length() > 0
-			&& !Objects.equals(student.getEmail(), email)) {
-			if(studentOptional.isPresent()) {
-				throw new IllegalStateException("email taken");
+		if(!studentUpdateDto.getEmail().isEmpty() && !studentUpdateDto.getEmail().isBlank()) {
+			if (this.studentRepository.existsByEmail(studentUpdateDto.getEmail()))
+				throw new DuplicateEntityException("Student with that email already exists.");
+			else {
+				student.setName(studentUpdateDto.getName());
+				student.setEmail(studentUpdateDto.getEmail());
+				student.setPhone(studentUpdateDto.getPhone());
+				student.setAddress(studentUpdateDto.getAddress());
+				student.setPicture(studentUpdateDto.getPicture());
+				log.info("Student with id {} has been updated.", studentId);
 			}
-			student.setEmail(email);
 		}
-		student.setPhone(phone);
-		student.setAddress(address);
-		student.setPicture(picture);
 
 	}
 }
--- src/main/java/com/example/tasksmanagementsystem/service/TaskService.java
+++ src/main/java/com/example/tasksmanagementsystem/service/TaskService.java
@@ -1,63 +1,80 @@
 package com.example.tasksmanagementsystem.service;
 
 import com.example.tasksmanagementsystem.dto.TaskUpdateDto;
+import com.example.tasksmanagementsystem.exception.DuplicateEntityException;
 import com.example.tasksmanagementsystem.model.Task;
+import com.example.tasksmanagementsystem.repository.StudentRepository;
 import com.example.tasksmanagementsystem.repository.TaskRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import javax.transaction.Transactional;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-public class TaskService {
+@Slf4j
+public class TaskService implements TaskInterface {
 	private final TaskRepository taskRepository;
+	private final StudentRepository studentRepository;
 
+	@Override
 	public List<Task> getTasks() {
 		return taskRepository.findAll();
 	}
 
-	public Optional<Task> getTaskById(Long taskId) {
-		return taskRepository.findTaskById(taskId);
+	@Override
+	public Task getTaskById(Long taskId) {
+		var task = this.taskRepository.findTaskById(taskId);
+		if (task.isEmpty()) {
+			throw new EntityNotFoundException("The task with the specified id was not found!");
+		}
+		return task.get();
 	}
 
-	public Optional<Task> getTaskByStudentId(Long studentId) {
-		return taskRepository.findTaskByStudentId(studentId);
-	}
-
-	public void createTask(Task task) {
-		Optional<Task> taskOptional = taskRepository
+	@Override
+	public Long createTask(Task task) {
+		var titleOptional = taskRepository
 			.findTaskByTitle(task.getTitle());
-		if(taskOptional.isPresent()) {
-			throw new IllegalStateException("Title is taken");
+		if(titleOptional.isPresent()) {
+			throw new DuplicateEntityException("User with this title already exists.");
 		}
 		taskRepository.save(task);
+		return task.getId();
 	}
 
+	@Override
 	public void deleteTask(Long taskId) {
 		boolean taskExists = taskRepository.existsById(taskId);
-		if(!taskExists) {
-			throw new IllegalStateException("Task by this id: " + taskId + " does not exist.");
+		if (!taskExists) {
+			throw new EntityNotFoundException("Task by this id: " + taskId + " does not exist.");
 		}
 		taskRepository.deleteById(taskId);
+		log.warn("Task with id {} , has been deleted.", taskId);
 	}
 
+	@Override
 	@Transactional
 	public void updateTask(Long taskId, TaskUpdateDto updatedTask) {
 		var taskFromDb = taskRepository.findById(taskId)
-			.orElseThrow(() -> new IllegalStateException(
+			.orElseThrow(() -> new EntityNotFoundException(
 				"task id " + taskId + "does not exists."
 			));
 		taskFromDb.setTitle(updatedTask.getTitle());
 		taskFromDb.setDescription(updatedTask.getDescription());
 
-//		return taskRepository.findById(taskId)
-//			.map(singleTask -> {
-//				singleTask.setTitle(task.getTitle());
-//				singleTask.setDescription(task.getDescription());
-//				return taskRepository.save(singleTask);
-//			});
 	}
+
+	@Override
+	@Transactional
+	public Task assignTask(Long taskId, Long studentId) {
+		var task = taskRepository.findTaskById(taskId).get();
+		var student = studentRepository.findById(studentId).get();
+		task.assignStudent(student);
+		return task;
+	}
+
+
 }
